93f7a17d239dfd0cb259b2636f8adf5921c347dd
Alif <alifahmed47@gmail.com>
2022-05-20 16:59:36 +0600

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import {
+  Button,
+  StyleSheet,
+  Text,
+  Image,
+  SafeAreaView,
+  TouchableHighlight,
+  Alert,
+} from 'react-native'
 
 export default function App() {
+  const alertMessage = () =>
+    Alert.alert('My Title', 'My message', [
+      { text: 'Yes', onPress: () => console.log('Yes') },
+      { text: 'No', onPress: () => console.log('No') },
+    ])
+
+  const alertPrompt = () =>
+    Alert.prompt('My Title', 'Me Message', (text) => console.log(text))
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <Text>Hello Worlds!</Text>
+      <Button
+        // onPress={onPressLearnMore}
+        title='Learn More'
+        color='#000'
+        accessibilityLabel='Learn more about this purple button'
+        onPress={alertPrompt}
+      />
+      <TouchableHighlight onLongPress={() => console.log('Image tapped!')}>
+        <Image
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: 'https://picsum.photos/200/300',
+          }}
+        />
+      </TouchableHighlight>
+
+      <StatusBar style='auto' />
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    marginTop: 20,
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
